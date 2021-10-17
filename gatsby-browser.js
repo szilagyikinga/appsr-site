58f28825080d555b3285cac85a8e9aabed0d9547
Szilagyi Kinga <szilagyikinga42@gmail.com>
2021-10-17 17:18:49 +0200

    feat: add blog page and background image plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
 import "@popperjs/core/dist/umd/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
+
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (!(`IntersectionObserver` in window)) {
+    import(`intersection-observer`);
+    console.log(`# IntersectionObserver is polyfilled!`);
+  }
+};
