9c39283fc140e37fa258bbb5e4fc6b5341c38daa
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-02-06 19:36:43 +0100

    feat: script for cookies

---
+++ src/html.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+      <footer>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.axeptioSettings = {
+              clientId: "61cc420c9ff8166ad2304f96",
+              cookiesVersion: "appsr-base",
+            };
+             
+            (function(d, s) {
+              var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+              e.async = true; e.src = "//static.axept.io/sdk-slim.js";
+              t.parentNode.insertBefore(e, t);
+            })(document, "script");
+    `,
+          }}
+        />
+      </footer>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
